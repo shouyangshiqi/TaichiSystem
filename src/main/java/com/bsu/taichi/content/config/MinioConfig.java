a00e972468ff700f57045248d379318512f46b95
khran <1090685251@qq.com>
2025-09-02 20:49:50 +0800

    媒体文件模块

---
+++ src/main/java/com/bsu/taichi/content/config/MinioConfig.java
@@ -0,0 +1,33 @@
+package com.bsu.taichi.content.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author khran
+ * @version 1.0
+ * @description
+ */
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.endpoint}")
+    private String minioEndpoint;
+
+    @Value("${minio.accessKey}")
+    private String minioAccessKey;
+
+    @Value("${minio.secretKey}")
+    private String minioSecretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioEndpoint)
+                .credentials(minioAccessKey, minioSecretKey)
+                .build();
+    }
+}
